e5705c27a8557474627801a43156511ad5943a73
NICOLASAREIZA <nicolasareiza93@gmail.com>
2024-02-23 11:43:19 -0500

    diez

--- src/pages/area.jsx
+++ src/pages/area.jsx
@@ -6,12 +6,49 @@ import MUIDataTable from "mui-datatables";
 
 export const Area = () => {
     const [modal, setModal] = useState(false)
+    const [usuarios, setArea] = useState([]);
+
+
+    useEffect(() => {
+        listarArea();
+    }, []);
+
+    const columns = [
+        "id_area",
+        "nombre"
+    ];
+
+    const options =
+    {
+        filterType:
+            'checkbox',
+    };
+
+
+    const listarArea = () => {
+        axios.get('http://localhost:4000/listar/Area')
+            .then(res => {
+                setArea(res.data)
+            })
+            .catch(err => {
+                console.error(err);
+            });
+    }
 
     return (
         <>
             <link rel="stylesheet" href="/public/css/maquinas.css" /><br />
             <div>
-                <MUIDataTable />
+                <MUIDataTable
+                title={"area"}
+
+                data={Area}
+
+                columns={columns}
+
+                options={options}
+                
+                />
             </div>
         </>
     )

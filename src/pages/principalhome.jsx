0e0344c2fb506a84574ca2784b2a7e0f0990a72e
NICOLASAREIZA <nicolasareiza93@gmail.com>
2024-03-08 09:59:38 -0500

    bobito

--- src/pages/principalhome.jsx
+++ src/pages/principalhome.jsx
@@ -8,6 +8,9 @@ export const Principalhome = () => {
     const [menu, setMenu] = useState(false);
     const [usuarios, setUsuarios] = useState([]);
     const [Maquina, setMaquina] = useState([]);
+    const [mantenimientos, setMantenimientos] = useState([]);
+    const [Ambiente, setAmbiente] = useState([]);
+    const [areas, setAreas] = useState([]);
 
     const listarUsuarios = () => {
         api.get('usuario/listar')
@@ -34,10 +37,46 @@ export const Principalhome = () => {
             });
     }
 
+    const listarMantenimiento = () => {
+        api.get('Mantenimiento/listar')
+            .then(res => {
+                setMantenimientos(res.data);
+            })
+            .catch(err => {
+                console.error(err);
+            });
+    }
+
+
+    const listarAmbiente = () => {
+        api.get('ambiente/listar')
+            .then(res => {
+                setAmbiente(res.data);
+            })
+            .catch(err => {
+                console.error(err);
+            });
+    };
+
+
+
+    const listarArea = () => {
+        api.get('Area/Listar')
+            .then(res => {
+                setAreas(res.data);
+            })
+            .catch(err => {
+                console.error(err);
+            });
+    };
+
 
       useEffect(()=>{
         listarUsuarios()
         listarMaquinas()
+        listarMantenimiento();
+        listarAmbiente();
+        listarArea();
       },[])
 
 
@@ -55,18 +94,17 @@ export const Principalhome = () => {
                 </div>
 
                 <div className="cuadro4">
-                    <h1>Mantenimientos  : 0</h1>
+                    <h1>Mantenimientos  : {mantenimientos.length}</h1>
                 </div>
 
                 <div className="cuadro5">
-                    <h1>Ambiente  : 0</h1>
+                    <h1>Ambiente  : {Ambiente.length}</h1>
                 </div>
 
                 <div className="cuadro6">
-                    <h1>Area  : 0</h1>
+                    <h1>Area  : {areas.length}</h1>
                 </div>
             </div>
-
             <Outlet>
             </Outlet>
 
